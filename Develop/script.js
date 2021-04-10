2deeb9d016de83686b9b4363b60178233d635da7
Dmitriy <dmitriy.babich0@gmail.com>
2021-04-10 15:45:00 -0700

    restarted from scratch because the old script was too complicated

--- Develop/script.js
+++ Develop/script.js
@@ -1,153 +1,106 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); // selects the generate button
 
-//VARIABLES
-var lower = false;
-var upper = false;
-var number = false;
-var symbol = false;
-
-
-
 
 //Password function 
-var valuesFunction = {
-  lower: getLower, 
+var generatorFunction = {
+  lower: getLower,
   upper: getUpper,
   number: getNumber,
   symbol: getSymbol
 };
 
+generateBtn.addEventListener('click', function(){
+    //Length prompt
+    function setLength(){
+        var lengthP = prompt('Input length of password: ');
+        if (lengthP < 8 ){
+            alert('Too low')
+            setLength();
+        } else if (lengthP > 128){
+            alert('Too high')
+            setLength();
+        } else {
+            length = lengthP
+            return length;
+        }
+    };
+    //Lower
+    function setLower(){
+        var lowerP = confirm('Lowercase in password: ');
+        if(lowerP){
+            lower = true;
+        } else {
+            lower = false;
+        }
+    };
+    //Upper
+    function setUpper(){
+        var upperP = confirm('Uppercase in password: ');
+        if(upperP){
+            upper = true;
+        } else {
+            upper = false;
+        }
+    };
+
+    function setNumber(){
+        var numberP = confirm('Numbers in password: ');
+        if(numberP){
+            number = true;
+        } else {
+            number = false;
+        }
+    };
+
+    function setSymbol(){
+        var symbolP = confirm('Symbols in password: ');
+        if(symbolP){
+            symbol = true;
+        } else {
+            symbol = false;
+        }
+    };
+
+
+    
+    setLength();
+    setLower();
+    setUpper();
+    setNumber();
+    setSymbol();
+});
 
 
 // generating functions
 function getLower(){
   return String.fromCharCode (Math.floor(Math.random() * 26) + 97);
-};
+}
 
 function getUpper(){
   return String.fromCharCode (Math.floor(Math.random() * 26) + 65);
-};
+}
 
 function getNumber(){
   return String.fromCharCode (Math.floor(Math.random() * 10) + 48);
-};
+}
 
 function getSymbol (){
   var symbols = '!@#$%^&*()-_=+,./<>?;'
   return symbols[Math.floor(Math.random() * symbols.length)];
-};
-
-
-// PORMPTS
-// password length prompt
-function pwLength () {
-  var length = prompt('Passowrd Length (it must be from 8-128 characters long): ');
-  if (length > 128) {
-    alert ('Too High')
-    pwLength();
-  } else if (length < 8) {
-    alert('Too low');
-    pwLength();
-  } else {
-    var passowdLength = length;
-  }
-};
-
-//Lower Case Prompt
-function lowerCase () {
-  var lowerCaseP = prompt('Do you want lower case letter in the password (Y/N) ');
-  if (lowerCaseP === 'N') {
-    lower= false;
-  } else if (lowerCaseP === 'Y') {
-    lower= true;
-  } else {
-    alert ('Please answer with a Y or a N')
-    lowerCase();
-  }
-};
-
-//Upper case prompt
-function upperCase () {
-  var upperCaseP = prompt('Do you want upper case letter in the password (Y/N) ');
-  if (upperCaseP === 'N') {
-    upper = false;
-  } else if (upperCaseP === 'Y') {
-    upper= true;
-  } else {
-    alert ('Please answer with a Y or a N')
-    upperCase();
-  }
-};
-
-//number prompt
-function numberCase () {
-  var numberCaseP = prompt('Do you want numbers in the password (Y/N) ');
-  if (numberCaseP === 'N') {
-    number = false;
-  } else if (numberCaseP === 'Y') {
-    number = true;
-  } else {
-    alert ('Please answer with a Y or a N')
-    numberCase();
-  }
-};
-
-//symbol prompt
-function symbolCase () {
-  var symbolCaseP = prompt('Do you want symbols in the password (Y/N) ');
-  if (symbolCaseP === 'N') {
-    symbol = false;
-  } else if (symbolCaseP === 'Y') {
-    symbol = true;
-  } else {
-    alert ('Please answer with a Y or a N')
-    symbolCase();
-  }
-};
-
-
-pwLength();
-lowerCase();
-upperCase();
-numberCase();
-symbolCase();
-
-//Prompt results
-
-function generatePassword (lower, upper, number, symbol, passwordLength){
-  var generatePassword = '';
-
-  var valuesCount = lower + upper + number + symbol;
-
-  console.log("types count: " + valuesCount);           //clg
-
-  var valuesArr = [{lower}, {upper}, {number}, {symbol}].filter(
-    item => Object.values(item)[0]                    // removes any keys with a false value
-  )
-  console.log("types Array ", valuesArr);               //clg
-
-  if (valuesCount === 0){                               // If nothing is selected
-    return '';
-  }
-};
-generatePassword(lower, upper, number, symbol, passwordLength);
-
-
-
+}
 
 // Write password to the #password input
 function writePassword() {                // function to write out the generated password
-   
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-};
+}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); // activates when you click on the generate password button
+// generateBtn.addEventListener("click", writePassword); // activates when you click on the generate password button
 
 
 
@@ -156,7 +109,7 @@ generateBtn.addEventListener("click", writePassword); // activates when you clic
 
 
 
- 
+
 // TO GENERATE PASSWORD I NEED:
 // #a function that uses math to generate a random number that is assigned to a lowecase latter of the alphabet
 // #a function that uses math to generate random uppercase letters 
@@ -166,8 +119,4 @@ generateBtn.addEventListener("click", writePassword); // activates when you clic
 // a way to select what i want and dont want in my generated passwords
 // validate that at least one of the generating functions is selected
 // a function to generate the password and that it matches my selected criteria
-// display the password in the textfield 
-
-
-
-// TEST
+// display the password in the textfield  
